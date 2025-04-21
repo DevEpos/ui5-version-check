4bef964f3717b61bc5677e63a409e5507c8a5716
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2025-04-21 22:01:11 +0200

    chore: add custom publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,15 @@
+echo "> Cleaning output"
+npm run clean
+
+echo "> Preparing build..."
+npm run build
+
+echo "> Copying additional sources"
+cp README.md LICENSE dist/src
+
+echo ">> Processing package.json"
+jq 'del(.scripts, .devDependencies)' package.json > dist/src/package.json
+
+echo "> Publishing to npm"
+cd ./dist/src
+npm publish
