f93917901bc4cd9531245ab0ee3cb050a5168256
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2025-05-07 20:18:40 +0200

    refactor: adjust exporte types

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,15 @@ import {
 } from "./lib/ui5-version-api";
 import { CheckSettings, UI5VersionCheck } from "./lib/ui5-version-check";
 import { getLogger, Logger, setLogger } from "./lib/utils";
-import { parseVersion, UI5VersionInfo, validateVersion, VersionValidator } from "./lib/version-validation";
+import {
+  parseVersion,
+  UI5VersionInfo,
+  validateVersion,
+  ValidationMessage,
+  ValidationResult,
+  VersionValidationOptions,
+  VersionValidator
+} from "./lib/version-validation";
 
 export {
   BaseVersionInfo,
@@ -18,9 +26,20 @@ export {
   latestVersion,
   parseVersion,
   setLogger,
+  UI5Version,
   UI5VersionCheck,
+  UI5VersionPatch,
   validateVersion,
   VersionValidator
 };
 
-export type { CheckSettings, Logger, ManifestCheckSummary, UI5Version, UI5VersionInfo, UI5VersionPatch, UI5Versions };
+export type {
+  CheckSettings,
+  Logger,
+  ManifestCheckSummary,
+  UI5VersionInfo,
+  UI5Versions,
+  ValidationMessage,
+  ValidationResult,
+  VersionValidationOptions
+};
